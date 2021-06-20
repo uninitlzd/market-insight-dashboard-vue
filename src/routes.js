8923aaba46450652697d05bac9a4b234eff40860
Alfredo Eka Wibowo <alfredoeka@outlook.com>
2021-06-21 01:14:01 +0700

    Change title and remove unused routes

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,8 @@
 import MarketInsight from './views/MarketInsight.vue'
-import About from './views/About.vue'
 import NotFound from './views/NotFound.vue'
-import PreviewComponent from './views/PreviewComponent.vue'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
   { path: '/', component: MarketInsight, meta: { title: 'Home' } },
-  {
-    path: '/about',
-    meta: { title: 'About' },
-    component: About,
-    // example of route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('./views/About.vue')
-  },
-  { path: '/prev', component: PreviewComponent },
   { path: '/:path(.*)', component: NotFound },
 ]
